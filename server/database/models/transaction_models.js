820f7f041d50d8adce9d546f567901268cd053f9
miguel whyte <whytemig@gmail.com>
2024-06-12 23:42:11 -0400

    transaction queries & mutation

--- server/database/models/transaction_models.js
+++ server/database/models/transaction_models.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const transactionModel = new mongoose.Schema(
   {
     userId: {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
