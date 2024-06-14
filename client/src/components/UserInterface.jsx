2824e5f7f1f03a828c1792aa94725faca90d49dd
miguel whyte <whytemig@gmail.com>
2024-06-14 01:13:19 -0400

    css update

--- client/src/components/UserInterface.jsx
+++ client/src/components/UserInterface.jsx
@@ -4,6 +4,9 @@ function UserInterface() {
       <div className="welcome-container border  border-gray-50 my-8 text-6xl text-center p-3">
         <h1>Welcome to Track-My-Money</h1>
       </div>
+      <button className="mb-3 bg-red-500 max-w-60 justify-center m-auto p-2 text-xl">
+        Tansaction History
+      </button>
       <div className="flex justify-between px-4 border  border-gray-50">
         <div>Charts</div>
         <div>Form</div>
