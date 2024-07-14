5ff4637301191197c1fde58a400b98b383a14b75
miguel whyte <whytemig@gmail.com>
2024-07-14 17:53:32 -0400

    cron job updated

--- server/cron.js
+++ server/cron.js
@@ -3,17 +3,17 @@ import https from "https";
 
 const URL = "https://etracker-tsio.onrender.com/";
 
-const job = new cron.CronJob("*/30****", () => {
+const job = new cron.CronJob("*/45 * * * *", function () {
   https
     .get(URL, (res) => {
       if (res.statusCode === 200) {
-        console.log("200 Get request");
+        console.log("GET request sent successfully");
       } else {
-        console.log("500 request");
+        console.log("GET request failed", res.statusCode);
       }
     })
     .on("error", (e) => {
-      console.log("Error while sending request", e);
+      console.error("Error while sending request", e);
     });
 });
 
